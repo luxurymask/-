c14c54505f1b3ebaf3ff7512f5d0b54e38da1825
liuxl <luxurymask@163.com>
2017-07-05 23:19:22 +0800

    .

---
+++ JVM/JVMTest/JVMTest.java
@@ -0,0 +1,15 @@
+public class JVMTest{
+	public String s;
+	public static String staticS = "sss";
+	public Object o;
+
+	public void function(int a, int b, Object o){
+		int i = 0;
+		int j = 32767;
+		String s = "123";
+		int k = i + j;
+		Object o1 = new Object();
+		this.o = o1;
+
+	}
+}
---
+++ JVM/JVMTest/LDCTest.java
@@ -0,0 +1,14 @@
+public class LDCTest{
+	public void function(){
+		int i = -1;
+		int j = 5;
+		int k = -2;
+		int l = 6;
+		int r = 127;
+		int z = 128;
+		int m = 32767;
+		int n = 32768;
+		int p = -32768;
+		int q = -32769;
+	}
+}
