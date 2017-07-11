d89a4796e61c00847ae3748361690092d1fc9630
liuxl <luxurymask@163.com>
2017-07-11 18:24:02 +0800

    .

---
+++ JVM/JVMTest/ClassTypeStoreTest.java
@@ -0,0 +1,14 @@
+public class ClassTypeStoreTest{
+	public void function(){
+		String s1 = "123";
+		String s2 = "abc";
+		int i1 = 123;
+		int i2 = 234;
+		long l1 = 123l;
+		long l2 = 345;
+		String sResult = s1 + s2;
+		int iResult = i1 + i2;
+		long lResult = l1 + l2;
+		String sResult2 = s1 + i1;
+	}
+}
---
+++ JVM/JVMTest/TryCatchFinallySyllableCodeTest.java
@@ -0,0 +1,11 @@
+public class TryCatchFinallySyllableCodeTest{
+	public void function(){
+		try{
+			int i = 0;
+		}catch(Exception e){
+			int j = 0;
+		}finally{
+			int k = 0;
+		}
+	}
+}
